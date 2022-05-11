a3763154d837e24998bd9d04441a1d7015383b20
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-05-11 11:28:22 +0100

    database added

--- app.py
+++ app.py
@@ -1,61 +1,38 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from werkzeug import exceptions
-# from flask_mail import Mail, Message
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
-CORS(app)
+app.config['SECRET_KEY'] = 'your secret key'
 
-# app.config['DEBUG'] = True
-# app.config['TESTING'] = False
-# app.config['MAIL_SERVER']='smtp.husmail.com'
-# app.config['MAIL_PORT'] = 587
-# app.config['MAIL_USERNAME'] = 'anisha@prettyprinted.com'
-# app.config['MAIL_PASSWORD'] = ''
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USE_SSL'] = False
-# # app.config['MAIL_DEBUG'] = True
-# app.config['AIL_DEFAULT_SENDER'] = 'anisha@prettyprinted.com'
-# app.config['MAIL_MAX_EMAILS'] = 5
-# # app.config['MAIL_SUPPRESS_SEND'] = False
-# app.config['MAIL_ASCI_ATTACHMENTS'] = False
-# mail = Mail(app)
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-
-
-# @app.route('/')
-# def index():
-#     msg = Message('Hello', recipients = ['jeyew26081@eoscast.com'])
-#     mail.send(msg)
-
-#     return 'Message has been sent'
 
 @app.route('/')
-def home():
-    return 'Welcome to our API. By Anisha, Ikenna and Sarushan'
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
-animes = [
-    {'id': 1, 'name': 'Bubble', 'year': 2022},
-    {'id': 2, 'name': 'Blackover', 'year': 2017},
-    {'id': 3, 'name': 'Blue Period', 'year': 2021}
-]
-@app.route('/anime', methods=['GET', 'POST'])
-def anime():
-    if request.method == 'GET':
-        return jsonify(animes)  
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
     if request.method == 'POST':
-        data = request.json
-        data['id'] = len(animes) + 1
-        animes.append(data)
-        return f"{data['name']} has been added to the anime list!"
+        title = request.form['title']
+        content = request.form['content']
 
-@app.errorhandler(exceptions.NotFound)
-def handle__404(err):
-    return jsonify({"message": f"Oops... {err}"}), 404
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
 
-@app.errorhandler(exceptions.InternalServerError)
-def handle_server_error(err):
-    return jsonify({"message": f"{err}. It's not you, it's us. Press F to pay respects"}), 500
-
-if __name__ == '__main__':
-    app.run()
+    return render_template('create.html')
